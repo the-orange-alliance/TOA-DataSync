b4ba0c6239fe6a7d797c7b71c65c1f47fc6e8e90
Ofek Ashery <ofek.ashery@gmail.com>
2020-10-22 00:24:32 +0300

    Improve MWC injection

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
@@ -12,7 +13,7 @@ module.exports = (env) => ({
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: './js/[name].bundle.js',
+    filename: './assets/js/[name].bundle.js',
     library: '[name]'
   },
   devServer: {
@@ -36,9 +37,11 @@ module.exports = (env) => ({
       dataSyncMode: JSON.stringify((env && env.NODE_ENV) || 'development')
     }),
     new CopyPlugin([
-      { from: './src/assets', to: './assets' },
-      { from: './node_modules/@mdi/font/fonts', to: './assets/mdi/fonts' },
-      { from: './node_modules/@mdi/font/css', to: './assets/mdi/css' }
+      { from: 'src/assets', to: 'assets/' },
+      { from: 'node_modules/@mdi/font/fonts', to: 'assets/fonts/' },
+      { from: 'node_modules/@mdi/font/css/materialdesignicons.min.css', to: 'assets/css/' },
+      { from: 'node_modules/material-components-web/dist/material-components-web.min.css', to: 'assets/css/' },
+      { from: 'node_modules/material-components-web/dist/material-components-web.min.js', to: 'assets/js/' }
     ]),
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -64,6 +67,15 @@ module.exports = (env) => ({
       template: './src/404.html',
       filename: '404.html',
       chunks: []
+    }),
+    new HtmlWebpackTagsPlugin({
+      links: [
+        '/assets/css/bootstrap.min.css',
+        '/assets/css/material-components-web.min.css',
+        '/assets/css/materialdesignicons.min.css'
+      ],
+      scripts: ['/assets/js/material-components-web.min.js'],
+      append: false
     })
   ]
 });
